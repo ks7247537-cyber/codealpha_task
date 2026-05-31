4dab6a2cdc65790fa9e8b5882b91d6b133fa2573
ks7247537-cyber <ks7247537@gmail.com>
2026-05-31 09:05:04 -0700

    Delete CodeAlpha_Stock_Portfolio_Tracker/CodeAlpha_Stock_Portfolio_Tracker directory

--- CodeAlpha_Stock_Portfolio_Tracker/CodeAlpha_Stock_Portfolio_Tracker/stock_portfolio_tracker/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Stock Portfolio Tracker package.
-
-A CodeAlpha internship project for managing stock portfolios
-with OOP design, CLI interface, and report generation.
-"""
-
-__version__ = "1.0.0"
-__author__ = "CodeAlpha Internship Project"
